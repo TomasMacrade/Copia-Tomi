b2dff0e5d58c6c4459b209c66bb8aac6a919ace5
Tomás Macrade <80725374+TomasMacrade@users.noreply.github.com>
2022-10-20 16:06:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const mensaje = document.querySelector("#mensaje");
 
 function encriptar(texto){
     var vocales = ["e","i","a","o","u"];
-    var vocalesEncriptadas = ["enter","imes","ai","ober","ufat"];
+    var vocalesEncriptadas = ["japón","imes","ai","ober","ufat"];
     texto = texto.toLowerCase();
 
     for(posicion = 0 ; posicion < vocales.length; posicion++){
